42e64c3c15ddfee2faade3d60f181f3c5544321d
Atul Varma <varmaa@gmail.com>
2011-12-18 15:32:55 -0500

    Made the utils namespace visible in ppx.js' own code, and also moved simpleRequestHeaders into a new config object.

--- ppx.js
+++ ppx.js
@@ -1,50 +1,115 @@
 var PPX = (function() {
-  function warn(msg) {
-    if (window.console && window.console.warn)
-      window.console.warn(msg);
-  }
-  
-  function absolutifyURL(url) {
-    var a = document.createElement('a');
-    a.setAttribute("href", url);
-    return a.href;
-  }
-  
-  function on(element, event, cb) {
-    if (element.attachEvent)
-      element.attachEvent("on" + event, cb);
-    else
-      element.addEventListener(event, cb, false);
-  }
-
-  function off(element, event, cb) {
-    if (element.detachEvent)
-      element.detachEvent("on" + event, cb);
-    else
-      element.removeEventListener(event, cb, false);
+  var config = {
+    requestHeaders: [
+      "Accept",
+      "Content-Type"
+    ]
   };
   
-  function encode(data) {
-    var parts = [];
-    for (var name in data) {
-      parts.push(name + "=" + encodeURIComponent(data[name]));
+  var utils = {
+    warn: function warn(msg) {
+      if (window.console && window.console.warn)
+        window.console.warn(msg);
+    },
+    absolutifyURL: function absolutifyURL(url) {
+      var a = document.createElement('a');
+      a.setAttribute("href", url);
+      return a.href;
+    },
+    on: function on(element, event, cb) {
+      if (element.attachEvent)
+        element.attachEvent("on" + event, cb);
+      else
+        element.addEventListener(event, cb, false);
+    },
+    off: function off(element, event, cb) {
+      if (element.detachEvent)
+        element.detachEvent("on" + event, cb);
+      else
+        element.removeEventListener(event, cb, false);
+    },
+    encode: function encode(data) {
+      var parts = [];
+      for (var name in data) {
+        parts.push(name + "=" + encodeURIComponent(data[name]));
+      }
+      return parts.join("&");
+    },
+    decode: function decode(string) {
+      return utils.parseUri("?" + string).queryKey;
+    },
+    isSameOrigin: function isSameOrigin(a, b) {
+      a = utils.parseUri(a);
+      b = utils.parseUri(b);
+      return (a.protocol == b.protocol && a.authority == b.authority);
+    },
+    map: function map(array, cb) {
+      var result = [];
+      for (var i = 0; i < array.length; i++)
+        result.push(cb(array[i]));
+      return result;
+    },
+    trim: function trim(str) {
+      return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+    },
+    // parseUri 1.2.2
+    // (c) Steven Levithan <stevenlevithan.com>
+    // MIT License
+    parseUri: function parseUri(str) {
+      var	o = utils.parseUriOptions,
+          m = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+          uri = {},
+          i = 14;
+
+      while (i--) uri[o.key[i]] = m[i] || "";
+
+      uri[o.q.name] = {};
+      uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+        if ($1) uri[o.q.name][$1] = decodeURIComponent($2);
+      });
+
+      return uri;
+    },
+    parseUriOptions: {
+      strictMode: false,
+      key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+      q: {
+        name:   "queryKey",
+        parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+      },
+      parser: {
+        strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+        loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+      }
+    },
+    // Taken from jQuery.
+    inArray: function inArray(elem, array, i) {
+      var len;
+      var indexOf = Array.prototype.indexOf;
+
+      if (array) {
+        if (indexOf) {
+          return indexOf.call(array, elem, i);
+        }
+
+        len = array.length;
+        i = i ? i < 0 ? Math.max(0, len + i) : i : 0;
+
+        for (;i < len; i++) {
+          // Skip accessing in sparse arrays
+          if (i in array && array[i] === elem) {
+            return i;
+          }
+        }
+      }
+
+      return -1;
     }
-    return parts.join("&");
-  }
+  };
   
-  function decode(string) {
-    return parseUri("?" + string).queryKey;
-  }
-
-  function isSameOrigin(a, b) {
-    a = parseUri(a);
-    b = parseUri(b);
-    return (a.protocol == b.protocol && a.authority == b.authority);
-  }
-
   function validateRequest(data, access, channel) {
-    var isValidMethod = (inArray(data.method.toUpperCase(),
-                                 access.allowMethods) != -1);
+    var isValidMethod = (utils.inArray(data.method.toUpperCase(),
+                                       access.allowMethods) != -1);
 
     if (!access.allowOrigin) {
       channel.error("CORS is unsupported at that path.");
@@ -63,25 +128,14 @@ var PPX = (function() {
     
     return true;
   }
-  
-  function map(array, cb) {
-    var result = [];
-    for (var i = 0; i < array.length; i++)
-      result.push(cb(array[i]));
-    return result;
-  }
 
-  function trim(str) {
-    return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-  }
-  
   function parseAccessControlHeaders(req) {
     function parseList(req, header) {
       var value = req.getResponseHeader(header);
       if (!value)
         return [];
       var list = value.split(",");
-      return map(list, trim);
+      return utils.map(list, utils.trim);
     }
     
     return {
@@ -89,64 +143,7 @@ var PPX = (function() {
       allowMethods: parseList(req, 'Access-Control-Allow-Methods')
     };
   }
-  
-  // parseUri 1.2.2
-  // (c) Steven Levithan <stevenlevithan.com>
-  // MIT License
 
-  function parseUri(str) {
-    var	o = parseUri.options,
-        m = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
-        uri = {},
-        i = 14;
-
-    while (i--) uri[o.key[i]] = m[i] || "";
-
-    uri[o.q.name] = {};
-    uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
-      if ($1) uri[o.q.name][$1] = decodeURIComponent($2);
-    });
-
-    return uri;
-  };
-
-  parseUri.options = {
-    strictMode: false,
-    key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
-    q: {
-      name:   "queryKey",
-      parser: /(?:^|&)([^&=]*)=?([^&]*)/g
-    },
-    parser: {
-      strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
-      loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
-    }
-  };
-
-  // Taken from jQuery.
-  function inArray(elem, array, i) {
-    var len;
-    var indexOf = Array.prototype.indexOf;
-
-    if (array) {
-      if (indexOf) {
-        return indexOf.call(array, elem, i);
-      }
-
-      len = array.length;
-      i = i ? i < 0 ? Math.max(0, len + i) : i : 0;
-
-      for (;i < len; i++) {
-        // Skip accessing in sparse arrays
-        if (i in array && array[i] === elem) {
-          return i;
-        }
-      }
-    }
-
-    return -1;
-  }
-	
   function SimpleChannel(other, onMessage, onError) {
     function getOtherWindow() {
       return other.postMessage ? other : other.contentWindow;
@@ -159,15 +156,15 @@ var PPX = (function() {
           window.console.error(message);
       },
       destroy: function() {
-        off(window, "message", messageHandler);
+        utils.off(window, "message", messageHandler);
         other = null;
         onMessage = null;
       },
       send: function(data) {
-        getOtherWindow().postMessage(encode(data), "*");
+        getOtherWindow().postMessage(utils.encode(data), "*");
       },
       error: function(message) {
-        getOtherWindow().postMessage(encode({
+        getOtherWindow().postMessage(utils.encode({
           __simpleChannelError: message
         }), "*");
       }
@@ -176,31 +173,20 @@ var PPX = (function() {
     function messageHandler(event) {
       if (event.source != getOtherWindow())
         return;
-      var data = decode(event.data);
+      var data = utils.decode(event.data);
       if ('__simpleChannelError' in data)
         self.onError(data.__simpleChannelError, event.origin);
       else
         self.onMessage(data, event.origin);
     }
     
-    on(window, "message", messageHandler);
+    utils.on(window, "message", messageHandler);
     return self;
   }
 
-  var simpleRequestHeaders = [
-    "Accept",
-    "Content-Type"
-  ];
-
   return {
-    utils: {
-      decode: decode,
-      encode: encode,
-      inArray: inArray,
-      isSameOrigin: isSameOrigin,
-      absolutifyURL: absolutifyURL
-    },
-    simpleRequestHeaders: simpleRequestHeaders,
+    utils: utils,
+    config: config,
     startServer: function startServer(options) {
       options = options || {};
 
@@ -209,9 +195,9 @@ var PPX = (function() {
         if (data.cmd == "send") {
           var req = new XMLHttpRequest();
           data.origin = origin;
-          data.headers = decode(data.headers);
+          data.headers = utils.decode(data.headers);
 
-          if (!isSameOrigin(window.location.href, data.url)) {
+          if (!utils.isSameOrigin(window.location.href, data.url)) {
             channel.error("url does not have same origin: " + data.url);
             return;
           }
@@ -238,7 +224,7 @@ var PPX = (function() {
           };
 
           for (var name in data.headers)
-            if (inArray(name, simpleRequestHeaders) == -1) {
+            if (utils.inArray(name, config.requestHeaders) == -1) {
               if (name == 'X-Requested-With') {
                 /* Just ignore jQuery's X-Requested-With header. */
               } else {
@@ -317,8 +303,8 @@ var PPX = (function() {
                 channel.send({
                   cmd: "send",
                   method: method,
-                  url: absolutifyURL(url),
-                  headers: encode(headers),
+                  url: utils.absolutifyURL(url),
+                  headers: utils.encode(headers),
                   body: body || ""
                 });
                 break;
@@ -336,7 +322,7 @@ var PPX = (function() {
                 break;
               }
             }, function onError(message) {
-              warn(message);
+              utils.warn(message);
               self.responseText = message;
               self.readyState = self.DONE;
               cleanup();
@@ -344,7 +330,7 @@ var PPX = (function() {
                 self.onreadystatechange();
             });
 
-            iframe.setAttribute("src", absolutifyURL(iframeURL));
+            iframe.setAttribute("src", utils.absolutifyURL(iframeURL));
             iframe.style.display = "none";
             document.body.appendChild(iframe);
           }
